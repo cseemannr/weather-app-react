1142a1119c4a4f7a7055e9bad5e598e1b2db9327
cseemannr <camilaseemannramos@gmail.com>
2023-11-21 22:19:23 -0300

    Favicon and footer href updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="container footer">
       <p className="opacity-75 code-link">
         <a
-          href="https://github.com/cseemannr/weather-app"
+          href="https://github.com/cseemannr/weather-app-react"
           target="_blank"
           className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
           rel="noreferrer"
